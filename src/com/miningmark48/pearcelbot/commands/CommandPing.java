ab80e9647b97c4c6871cfbe31d009bf1a6d589c3
MiningMark48 <miningmark48@gmail.com>
2018-03-04 22:49:28 -0500

    Ping sanity check

--- src/com/miningmark48/pearcelbot/commands/CommandPing.java
+++ src/com/miningmark48/pearcelbot/commands/CommandPing.java
@@ -20,7 +20,7 @@ public class CommandPing implements ICommand, ICommandPrivate {
 
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
-        event.getTextChannel().sendMessage("\uD83C\uDFD3 **Pong! **...").queue(m -> m.editMessage("\uD83C\uDFD3 **Pong! **" + event.getMessage().getCreationTime().until(m.getCreationTime(), ChronoUnit.MILLIS) + "ms \uD83D\uDCF6").queue());
+        event.getTextChannel().sendMessage("\uD83C\uDFD3 **Pong! **...").queue(m -> m.editMessage("\uD83C\uDFD3 **Pong! **" + Math.abs(event.getMessage().getCreationTime().until(m.getCreationTime(), ChronoUnit.MILLIS)) + "ms \uD83D\uDCF6").queue());
     }
 
     @Override
