42811d1ff0e4437693aa0a0b2ae538c214934b71
MiningMark48 <miningmark48@gmail.com>
2018-03-19 15:51:41 -0400

    Added method for registering commands

--- src/com/miningmark48/pearcelbot/commands/base/InitializeCommands.java
+++ src/com/miningmark48/pearcelbot/commands/base/InitializeCommands.java
@@ -14,88 +14,92 @@ public class InitializeCommands {
         Main.commands.clear();
 
         //Init
-        Main.commands.put("8ball", new Command8Ball());
-        Main.commands.put("addcommand", new CommandAddCommand());
-        Main.commands.put("announce", new CommandAnnounce());
-        Main.commands.put("bark", new CommandBark());
-//        Main.commands.put("bitcoin", new CommandBitcoin());
-        Main.commands.put("arblacklist", new CommandARBlacklist());
-        Main.commands.put("botinfo", new CommandBotInfo());
-        Main.commands.put("botstats", new CommandBotStats());
-        Main.commands.put("cmds", new CommandCmds());
-        Main.commands.put("coinflip", new CommandCoinFlip());
-        Main.commands.put("cursedata", new CommandCurseData());
-        Main.commands.put("deletecommand", new CommandDeleteCommand());
-        Main.commands.put("diceroll", new CommandDiceRoll());
-        Main.commands.put("editcommand", new CommandEditCommand());
-        Main.commands.put("emojieval", new CommandEmojiEval());
-        Main.commands.put("fizzbuzz", new CommandFizzbuzz());
-        Main.commands.put("geoip", new CommandGeoIP());
-        Main.commands.put("githubuser", new CommandGithubUser());
-        Main.commands.put("guildinfo", new CommandGuildInfo());
-        Main.commands.put("help", new CommandHelp());
-        Main.commands.put("kickvoice", new CommandKickVoice());
-        Main.commands.put("listbots", new CommandListBots());
-        Main.commands.put("listcommands", new CommandListCommands());
-        Main.commands.put("lmgtfy", new CommandLMGTFY());
-        Main.commands.put("mixeruser", new CommandMixerUser());
-        Main.commands.put("numtoword", new CommandNumberToWords());
-        Main.commands.put("patreon", new CommandPatreon());
-        Main.commands.put("ping", new CommandPing());
-        Main.commands.put("poll", new CommandPoll());
-        Main.commands.put("prune", new CommandPrune());
-        Main.commands.put("roles", new CommandRoles());
-        Main.commands.put("rps", new CommandRPS());
-        Main.commands.put("say", new CommandSay());
-        Main.commands.put("selfinfo", new CommandSelfInfo());
-        Main.commands.put("serverinvite", new CommandServerInvite());
-        Main.commands.put("slap", new CommandSlap());
-        Main.commands.put("sourcecode", new CommandSourceCode());
-        Main.commands.put("tobinary", new CommandToBinary());
-        Main.commands.put("toromannum", new CommandToRomanNum());
-        Main.commands.put("translate", new CommandTranslate());
-        Main.commands.put("twitchuser", new CommandTwitchUser());
-        Main.commands.put("uptime", new CommandUptime());
-        Main.commands.put("youtubeuser", new CommandYoutubeUser());
+        registerCommand("8ball", new Command8Ball());
+        registerCommand("addcommand", new CommandAddCommand());
+        registerCommand("announce", new CommandAnnounce());
+        registerCommand("bark", new CommandBark());
+//        registerCommand("bitcoin", new CommandBitcoin());
+        registerCommand("arblacklist", new CommandARBlacklist());
+        registerCommand("botinfo", new CommandBotInfo());
+        registerCommand("botstats", new CommandBotStats());
+        registerCommand("cmds", new CommandCmds());
+        registerCommand("coinflip", new CommandCoinFlip());
+        registerCommand("cursedata", new CommandCurseData());
+        registerCommand("deletecommand", new CommandDeleteCommand());
+        registerCommand("diceroll", new CommandDiceRoll());
+        registerCommand("editcommand", new CommandEditCommand());
+        registerCommand("emojieval", new CommandEmojiEval());
+        registerCommand("fizzbuzz", new CommandFizzbuzz());
+        registerCommand("geoip", new CommandGeoIP());
+        registerCommand("githubuser", new CommandGithubUser());
+        registerCommand("guildinfo", new CommandGuildInfo());
+        registerCommand("help", new CommandHelp());
+        registerCommand("kickvoice", new CommandKickVoice());
+        registerCommand("listbots", new CommandListBots());
+        registerCommand("listcommands", new CommandListCommands());
+        registerCommand("lmgtfy", new CommandLMGTFY());
+        registerCommand("mixeruser", new CommandMixerUser());
+        registerCommand("numtoword", new CommandNumberToWords());
+        registerCommand("patreon", new CommandPatreon());
+        registerCommand("ping", new CommandPing());
+        registerCommand("poll", new CommandPoll());
+        registerCommand("prune", new CommandPrune());
+        registerCommand("roles", new CommandRoles());
+        registerCommand("rps", new CommandRPS());
+        registerCommand("say", new CommandSay());
+        registerCommand("selfinfo", new CommandSelfInfo());
+        registerCommand("serverinvite", new CommandServerInvite());
+        registerCommand("slap", new CommandSlap());
+        registerCommand("sourcecode", new CommandSourceCode());
+        registerCommand("tobinary", new CommandToBinary());
+        registerCommand("toromannum", new CommandToRomanNum());
+        registerCommand("translate", new CommandTranslate());
+        registerCommand("twitchuser", new CommandTwitchUser());
+        registerCommand("uptime", new CommandUptime());
+        registerCommand("youtubeuser", new CommandYoutubeUser());
 
         //Me only
-        Main.commands.put("getdemservs", new CommandGetDemServs());
+        registerCommand("getdemservs", new CommandGetDemServs());
 
         //Music
-        Main.commands.put("play", new CommandPlay());
-        Main.commands.put("pplay", new CommandPlayPlaylist());
-        Main.commands.put("splay", new CommandPlaySearch());
-        Main.commands.put("splayrand", new CommandPlaySearchRand());
-        Main.commands.put("splayremix", new CommandPlaySearchRemix());
-        Main.commands.put("skip", new CommandSkip());
-        Main.commands.put("stop", new CommandStop());
-        Main.commands.put("queue", new CommandQueue());
-        Main.commands.put("pause", new CommandPause());
-        Main.commands.put("playnext", new CommandPlayNext());
-        Main.commands.put("remove", new CommandRemove());
-        Main.commands.put("repeat", new CommandRepeat());
-        Main.commands.put("repeatonce", new CommandRepeatOnce());
-        Main.commands.put("resume", new CommandResume());
-        Main.commands.put("nowplaying", new CommandNowPlaying());
-        Main.commands.put("shuffle", new CommandShuffle());
-        Main.commands.put("lyrics", new CommandLyrics());
-        Main.commands.put("restart", new CommandRestart());
-        Main.commands.put("summon", new CommandSummon());
-        Main.commands.put("voteskip", new CommandVoteSkip());
-        Main.commands.put("clear", new CommandClear());
+        registerCommand("play", new CommandPlay());
+        registerCommand("pplay", new CommandPlayPlaylist());
+        registerCommand("splay", new CommandPlaySearch());
+        registerCommand("splayrand", new CommandPlaySearchRand());
+        registerCommand("splayremix", new CommandPlaySearchRemix());
+        registerCommand("skip", new CommandSkip());
+        registerCommand("stop", new CommandStop());
+        registerCommand("queue", new CommandQueue());
+        registerCommand("pause", new CommandPause());
+        registerCommand("playnext", new CommandPlayNext());
+        registerCommand("remove", new CommandRemove());
+        registerCommand("repeat", new CommandRepeat());
+        registerCommand("repeatonce", new CommandRepeatOnce());
+        registerCommand("resume", new CommandResume());
+        registerCommand("nowplaying", new CommandNowPlaying());
+        registerCommand("shuffle", new CommandShuffle());
+        registerCommand("lyrics", new CommandLyrics());
+        registerCommand("restart", new CommandRestart());
+        registerCommand("summon", new CommandSummon());
+        registerCommand("voteskip", new CommandVoteSkip());
+        registerCommand("clear", new CommandClear());
 
         //Alias
-//        Main.commands.put("help", new CommandCmds());
-//        Main.commands.put("addcmd", new CommandAddCommand());
-//        Main.commands.put("curseforge", new CommandCurseData());
-//        Main.commands.put("delcmd", new CommandDeleteCommand());
-//        Main.commands.put("listcmds", new CommandListCommands());
+//        registerCommand("help", new CommandCmds());
+//        registerCommand("addcmd", new CommandAddCommand());
+//        registerCommand("curseforge", new CommandCurseData());
+//        registerCommand("delcmd", new CommandDeleteCommand());
+//        registerCommand("listcmds", new CommandListCommands());
 //            //Music
-//        Main.commands.put("np", new CommandNowPlaying());
-//        Main.commands.put("sb", new CommandSoundboard());
+//        registerCommand("np", new CommandNowPlaying());
+//        registerCommand("sb", new CommandSoundboard());
 
-        LoggerUtil.log(LoggerUtil.LogType.STATUS, "Commands Initialized.");
+        LoggerUtil.log(LoggerUtil.LogType.STATUS, Main.commands.size() + " Commands Initialized.");
 
+    }
+    
+    private static void registerCommand(String trigger, ICommand command) {
+        Main.commands.put(trigger, command);
     }
 
 }
