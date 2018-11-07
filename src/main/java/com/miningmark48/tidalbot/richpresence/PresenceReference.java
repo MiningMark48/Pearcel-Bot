ea7b58b8dff837712a5339b234bd62c892132ec5
Mark <miningmark48@gmail.com>
2018-11-07 01:09:50 -0500

    Normal Presence

--- src/main/java/com/miningmark48/tidalbot/richpresence/PresenceReference.java
+++ src/main/java/com/miningmark48/tidalbot/richpresence/PresenceReference.java
@@ -4,8 +4,14 @@ import com.miningmark48.tidalbot.reference.Reference;
 
 public class PresenceReference {
 
+    //Normal
     public static String[] playingPresence = {"Do " + Reference.botCommandKey + "cmds", "Pearcel Mod", "the banjo", "Minecraft", "Overwatch"};
     public static String[] listeningPresence = {"sick music", "Marshmello", "San Holo", "EDM", "electro", "Blake Shelton", "bluegrass", "nagging"};
     public static String[] watchingPresence = {"movies", "airplanes", "you", "YouTube", "kaosjr stream", "boring dev streams"};
+
+    //Halloween
+//    public static String[] playingPresence = {"Friday the 13th: The Game", "5 Nights at Freddy's", "Resident Evil", "Silent Hill", "Slender: The Eight Pages"};
+//    public static String[] listeningPresence = {"\uD83C\uDF83", "Hells Bells", "This is Halloween", "Demons", "Thriller"};
+//    public static String[] watchingPresence = {"Halloween", "Friday the 13th", "Paranormal Activity", "A Quiet Place", "Ghostbusters"};
 
 }
